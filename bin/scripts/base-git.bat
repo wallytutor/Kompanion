03681011dae0b5d24baf2b4e8bb46f2fb3d5d24a
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-01-29 19:20:36 +0100

    Fixed names

--- bin/scripts/base-git.bat
+++ bin/scripts/base-git.bat
@@ -1,8 +1,13 @@
+@REM ****
+@REM IMPORTANT: do not change the name of these variables! Using the
+@REM same standard as the rest of the project will break PortableGit!
+@REM ****
+
 @REM Configure version (name of directory under apps/):
-set GIT_DIR=PortableGit
+set THE_GIT_DIR=PortableGit
 
 @REM Path to executables root:
-set GIT_HOME=%KOMPANION_APPS%\%GIT_DIR%
+set THE_GIT_HOME=%KOMPANION_APPS%\%THE_GIT_DIR%
 
 @REM Add to path:
-set PATH=%GIT_HOME%\cmd;%PATH%
+set PATH=%THE_GIT_HOME%\cmd;%PATH%
