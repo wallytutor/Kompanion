aa248abc4391bcf41813a845e07efd68b8534b63
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-01-29 18:22:23 +0100

    Standardize

--- bin/scripts/base-git.bat
+++ bin/scripts/base-git.bat
@@ -1,5 +1,8 @@
 @REM Configure version (name of directory under apps/):
 set GIT_DIR=PortableGit
 
+@REM Path to executables root:
+set GIT_HOME=%KOMPANION_APPS%\%GIT_DIR%
+
 @REM Add to path:
-set PATH=%KOMPANION_APPS%\%GIT_DIR%\cmd;%PATH%
+set PATH=%GIT_HOME%\cmd;%PATH%
