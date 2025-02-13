e442cf243cf9d53b8dcc8fbeb7525ce9ef3ef587
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-02-13 12:13:26 +0100

    Updates

--- bin/scripts/base-python.bat
+++ bin/scripts/base-python.bat
@@ -6,3 +6,7 @@ set PYTHON_HOME=%KOMPANION_APPS%\%PYTHON_DIR%\python
 
 @REM Add to path:
 set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Scripts;%PATH%
+
+@REM Add modules to path:
+set PATH=%KOMPANION_PKGS%\Taskforce.jl\src\py;%PATH%
+set PYTHONPATH=%KOMPANION_PKGS%\Taskforce.jl\src\py;%PYTHONPATH%
