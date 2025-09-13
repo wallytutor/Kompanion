ea334647189497341153899629665713d82c1ed2
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-09-13 08:45:05 +0200

    Add quicklisp path

--- bin/scripts/base-portacle.bat
+++ bin/scripts/base-portacle.bat
@@ -1,6 +1,9 @@
 @REM Configure version (the name of directory under apps/):
 set PORTACLE_DIR=portacle
 
+@REM Path to quicklisp package manager:
+set QUICKLISP_HOME=%KOMPANION_APPS%\%PORTACLE_DIR%\all\quicklisp
+
 @REM Path to executables root:
 set PORTACLE_HOME=%KOMPANION_APPS%\%PORTACLE_DIR%\win
 
