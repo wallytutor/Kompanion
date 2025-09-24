cbcd58b2dea32ca8424b98224730837e82901a41
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-09-24 21:55:27 +0200

    Stripping legacy

--- legacy/bin/scripts/extra-gnuplot.bat
+++
@@ -1,8 +0,0 @@
-@REM Configure version (the name of directory under apps/):
-set GNUPLOT_DIR=gnuplot
-
-@REM Path to executables root:
-set GNUPLOT_HOME=%KOMPANION_APPS%\%GNUPLOT_DIR%
-
-@REM Add to path:
-set PATH=%GNUPLOT_HOME%\bin;%PATH%
--- legacy/bin/scripts/extra-graphviz.bat
+++
@@ -1,9 +0,0 @@
-@REM Configure version (the name of directory under apps/):
-set GRAPHVIZ_DIR=Graphviz-12.2.1-win64
-
-@REM Path to executables root:
-set GRAPHVIZ_HOME=%KOMPANION_APPS%\%GRAPHVIZ_DIR%
-
-@REM Add to path:
-@REM set PATH=%GRAPHVIZ_HOME%\bin;%GRAPHVIZ_HOME%\lib;%PATH%
-set PATH=%GRAPHVIZ_HOME%\bin;%PATH%
